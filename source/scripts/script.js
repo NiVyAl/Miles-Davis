58f13097c94a80f50e3c4fd8d15aa53d5eef6b31
nick <nikitavisoc@gmail.com>
2019-02-04 23:14:39 +0300

    подправил стили, js

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,12 +1,17 @@
 //console.log(window.pageYOffset);
 
 var mentionsList = document.querySelector('.mentions__list');
+var avatar = document.querySelectorAll('.mention__author-avatar');
 
 window.onscroll = function() {
   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
   
   if (scrolled > 370) {
     mentionsList.classList.add("mentions__list--mini");
+    for (var i=0; i < avatar.length; i++) {
+      avatar[i].style.top = scrolled-370 + "px";
+    }
+    
   } else {
     mentionsList.classList.remove("mentions__list--mini");
   }
